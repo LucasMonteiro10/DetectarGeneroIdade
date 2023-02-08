12a106e6a23150858874d983659250e36ae6b3a4
LucasMonteiro10 <lmvb@discente.ifpe.edu.br>
2023-02-07 22:53:15 -0300

    Adiciona pasta para guardar as imagens da aplicação

--- script2.js
+++ script2.js
@@ -27,19 +27,19 @@ function verificar()
             genero = 'Homem';
             if(idade >= 0 && idade < 10)        //Criança
             {
-                img.setAttribute('src', 'foto-bebe-m.png');
+                img.setAttribute('src', 'images/foto-bebe-m.png');
             }
             else if(idade >=10 && idade < 21)   //Jovem
             {
-                img.setAttribute('src', 'foto-jovem-m.png');
+                img.setAttribute('src', 'images/foto-jovem-m.png');
             }
             else if(idade < 50)                 //Adulto
             {
-                img.setAttribute('src', 'foto-adulto-m.png');
+                img.setAttribute('src', 'images/foto-adulto-m.png');
             }
             else                                //Idoso
             {
-                img.setAttribute('src', 'foto-idoso-m.png');
+                img.setAttribute('src', 'images/foto-idoso-m.png');
             }
         }
         else if (fsex[1].checked)
@@ -48,19 +48,19 @@ function verificar()
             genero = 'Mulher';
             if(idade >= 0 && idade < 10)        //Criança
             {
-                img.setAttribute('src', 'foto-bebe-f.png');
+                img.setAttribute('src', 'images/foto-bebe-f.png');
             }
             else if(idade >=10 && idade < 21)   //Jovem
             {
-                img.setAttribute('src', 'foto-jovem-f.png');
+                img.setAttribute('src', 'images/foto-jovem-f.png');
             }
             else if(idade < 50)                 //Adulto
             {
-                img.setAttribute('src', 'foto-adulto-f.png');
+                img.setAttribute('src', 'images/foto-adulto-f.png');
             }
             else                                //Idoso
             {
-                img.setAttribute('src', 'foto-idoso-f.png');
+                img.setAttribute('src', 'images/foto-idoso-f.png');
             }
         }
         
